cdb7b38e1996de6c43bc851b1348afc6a31db644
Rupert Jabelman <rupert@jabelman.org>
2024-11-20 11:49:06 +0000

    Code style.

--- src/Form/PublicationImportForm.php
+++ src/Form/PublicationImportForm.php
@@ -4,29 +4,21 @@ namespace Drupal\localgov_publications_importer\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
 use Drupal\localgov_publications_importer\Service\Importer as PublicationImporter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\file\Entity\File;
+
 /**
  * Publication import form.
  */
 class PublicationImportForm extends FormBase {
 
   /**
-   * Publication importer service
-   *
-   * @var \Drupal\localgov_publications_importer\Service\Importer
-   */
-  protected $publicationImporter;
-
-  /**
    * Constructor.
-   *
-   * @param \Drupal\localgov_publications_importer\Service\Importer $publicationImporter
-   *   Publication importer.
    */
-  public function __construct(PublicationImporter $publicationImporter) {
-    $this->publicationImporter = $publicationImporter;
+  public function __construct(
+    protected PublicationImporter $publicationImporter,
+  ) {
   }
 
   /**
@@ -56,13 +48,13 @@ class PublicationImportForm extends FormBase {
     $form['my_file'] = array(
       '#type' => 'managed_file',
       '#name' => 'my_file',
-      '#title' => t('File *'),
+      '#title' => $this->t('File *'),
       '#size' => 20,
-      '#description' => t('PDF format only'),
+      '#description' => $this->t('PDF format only'),
       '#upload_validators' => [
         'file_validate_extensions' => ['pdf'],
       ],
-      // @todo: Upload to private.
+      // @todo Upload to private.
       '#upload_location' => 'public://my_files/',
     );
 
@@ -81,19 +73,15 @@ class PublicationImportForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    // Need to get file details i.e upload file name, size etc.
-
     [$fid] = $form_state->getValue('my_file');
     $file = File::load($fid);
 
     $node = $this->publicationImporter->importPdf($file->uri->value);
 
     if ($node) {
-      // redirect to the node...
+      // Redirect to the node we created.
       $form_state->setRedirect('entity.node.canonical', ['node' => $node->id()]);
     }
-
-    // Else set a fail message
   }
 
 }

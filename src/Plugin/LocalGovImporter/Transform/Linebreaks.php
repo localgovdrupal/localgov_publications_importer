553ec9bdd3a07ac2154cf70d224489b704b89264
Rupert Jabelman <rupert@jabelman.org>
2025-11-04 17:02:47 +0000

    Remove last implementation of TransformInterface::order().

--- src/Plugin/LocalGovImporter/Transform/Linebreaks.php
+++ src/Plugin/LocalGovImporter/Transform/Linebreaks.php
@@ -22,11 +22,4 @@ class Linebreaks extends TransformPluginBase {
     return trim(str_replace("\t\n", ' ', $content));
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function order(): int {
-    return 30;
-  }
-
 }
